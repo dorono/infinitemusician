aa5812ca76d472d729a3c45747ce32de1e03f2cb
Doron <me@doronorenstein.com>
2016-09-04 18:48:21 -0500

    got JS selectors right, still need to complete function to display product info

--- plugins/theme-customisations-master/custom/custom.js
+++ plugins/theme-customisations-master/custom/custom.js
@@ -1,7 +1,12 @@
 jQuery(document).ready(function ($) {
-    $(document).on('click', function (evt) {
+    $(document).find('.woocommerce-checkout').on('click', function (evt) {
       console.log('evt', evt.target.className);
-      var anchorEl = $(this).find('.product-remove a');
-      console.log('this is it yo', anchorEl.attr('class'));
+      var linkClass = evt.target.className;
+      var anchorEl = $(this).find('.product-remove a').attr('class');
+
+      if (linkClass === anchorEl) {
+        console.log('this is it');
+      }
+
     });
 });
