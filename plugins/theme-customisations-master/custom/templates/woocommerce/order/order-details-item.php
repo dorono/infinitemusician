c5be598b66db66bfc9017c9a22df13cb077863a4
Doron <me@doronorenstein.com>
2016-10-26 19:24:48 -0500

    made order review page dynamic

--- plugins/theme-customisations-master/custom/templates/woocommerce/order/order-details-item.php
+++ plugins/theme-customisations-master/custom/templates/woocommerce/order/order-details-item.php
@@ -24,8 +24,8 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 	return;
 }
 ?>
-<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ); ?>">
-	<td class="product-name">
+</table>
+<div class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ); ?>">
 		<?php
 			$is_visible        = $product && $product->is_visible();
 			$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
@@ -40,13 +40,16 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 
 			do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order );
 		?>
-	</td>
-	<td class="product-total">
-		<?php echo $order->get_formatted_line_subtotal( $item ); ?>
-	</td>
-</tr>
+
+		<h3><?php echo $order->get_formatted_line_subtotal( $item ); ?></h3>
+</div>
+
+
 <?php if ( $show_purchase_note && $purchase_note ) : ?>
-<tr class="product-purchase-note">
-	<td colspan="3"><?php echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) ); ?></td>
-</tr>
+
+<div class="product-purchase-note">
+	<?php echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) ); ?>
+</div>
+
 <?php endif; ?>
+<table class="shop_table customer_details">
