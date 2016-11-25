4025c78d693fdc3b81288d4fdbf4c030ced68ef4
Doron <me@doronorenstein.com>
2016-11-24 19:02:49 -0600

    added FB tracking pixel

--- themes/homestore/header.php
+++ themes/homestore/header.php
@@ -15,6 +15,25 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '909075279152522', {
+em: 'insert_email_variable,'
+});
+fbq('track', 'PageView');
+fbq('track', 'Purchase', {value: '34.95', currency:'USD'});
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=909075279152522&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
 </head>
 
 <body <?php body_class(); ?>>
